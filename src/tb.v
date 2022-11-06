6c97e6477f64797d7f3d8ca36b72729bcd9f687f
Jan Gray <jan@fpga.org>
2022-11-06 15:14:37 -0500

    tabs=>spaces

--- src/tb.v
+++ src/tb.v
@@ -7,20 +7,20 @@ that can be driven / tested by the cocotb test.py
 */
 
 module tb(
-	input wire			clk,
-	input wire			rst,
-	input wire [3:0]	si,
-	output wire [7:0]	luts
+    input wire          clk,
+    input wire          rst,
+    input wire [3:0]    si,
+    output wire [7:0]   luts
 );
-	initial begin
-		$dumpfile ("tb.vcd");
-		$dumpvars (0, tb);
-		#1;
-	end
+    initial begin
+        $dumpfile ("tb.vcd");
+        $dumpvars (0, tb);
+        #1;
+    end
 
-	wire [7:0] io_in = {2'b0, si, rst, clk};
-	wire [7:0] io_out;
-	assign luts = io_out;
+    wire [7:0] io_in = {2'b0, si, rst, clk};
+    wire [7:0] io_out;
+    assign luts = io_out;
 
-	S4GA #(.N(16), .K(4), .SI_W(4)) s4ga(.io_in, .io_out);
+    S4GA #(.N(16), .K(4), .SI_W(4)) s4ga(.io_in, .io_out);
 endmodule
