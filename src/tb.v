a5b2a116a4dc470314a126e93aac7ec24af9a2d9
Jan Gray <jan@fpga.org>
2022-11-06 15:37:53 -0500

    try N=32 K=4-LUTs

--- src/tb.v
+++ src/tb.v
@@ -1,11 +1,6 @@
 `default_nettype none
 `timescale 1ns/1ps
 
-/*
-this testbench just instantiates the module and makes some convenient wires
-that can be driven / tested by the cocotb test.py
-*/
-
 module tb(
     input wire          clk,
     input wire          rst,
@@ -22,5 +17,5 @@ module tb(
     wire [7:0] io_out;
     assign luts = io_out;
 
-    s4ga #(.N(16), .K(4), .SI_W(4)) s4ga(.io_in, .io_out);
+    s4ga s4ga(.io_in, .io_out);
 endmodule
