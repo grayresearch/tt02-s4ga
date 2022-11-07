bef4631a97cb87b3016a536c98e7b0b4bbac8629
Jan Gray <jan@fpga.org>
2022-11-07 06:22:33 -0500

    experiment: try N=64 K=5-LUTs

--- src/s4ga.v
+++ src/s4ga.v
@@ -30,7 +30,7 @@
 // };
 module s4ga #(
     parameter int N     = 64,   // # LUTs
-    parameter int K     = 4,    // # LUT inputs
+    parameter int K     = 5,    // # LUT inputs
     parameter int SI_W  = 4     // SI width
 ) (
     input  wire `V(8)   io_in,
