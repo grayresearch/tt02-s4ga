610a39e0c4c354d34155c4a1c8667e3d399bdbb1
Jan Gray <jan@fpga.org>
2022-11-21 22:06:06 -0800

    N=89

--- src/test.py
+++ src/test.py
@@ -8,7 +8,7 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-N = 67              # no. LUTs
+N = 89              # no. LUTs
 K = 5               # no. LUT inputs
 I = 2               # no. FPGA inputs
 W = 4               # LUT config data segment width
@@ -95,7 +95,16 @@ vectors=[
     zero, zero, zero, zero, zero,
     zero, zero, zero, zero, zero,
 
-#60: test O=7 output pins: output 'b1010101
+#60:
+    zero, zero, zero, zero, zero,
+    zero, zero, zero, zero, zero,
+
+#70:
+    zero, zero, zero, zero, zero,
+    zero, zero, zero, zero, zero,
+
+#80: test O=7 output pins: output 'b1010101
+	zero, zero,
     [H,_,_,_,_, _E, 0,0,  1,0,0,0,0, 1], # _E(1,...) = 1
     [_,_,_,_,_, _E, 0,0,  0,0,0,0,0, 0], # _E(0,...) = 0
     [H,_,_,_,_, _E, 0,0,  1,0,0,0,0, 1], # _E(1,...) = 1
